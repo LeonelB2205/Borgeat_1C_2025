5d3c170afb0ad289ce800374440257d23e555633
Leonel Borgeat <leonel.borgeat@ingenieria.uner.edu.ar>
2025-06-06 16:21:57 -0300

    Agrego el proyecto libre de pulsadores led,con driver bluetooth

--- firmware/projects/ProyectoLibre/main/ProyectoLibre.c
+++ firmware/projects/ProyectoLibre/main/ProyectoLibre.c
@@ -11,7 +11,13 @@
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | 	LED1	 	| 	GPIO_10		|
+ * | 	LED2	 	| 	GPIO_11		|
+ * | 	LED3	 	| 	GPIO_5		|
+ * | 	Pulsador1	| 	GPIO_1		|
+ * | 	Pulsador2	| 	GPIO_2		|
+ * | 	Pulsador3	| 	GPIO_3		|
+ *
  *
  *
  * @section changelog Changelog
@@ -31,54 +37,172 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "gpio_mcu.h"
-#include "led.h"
 #include "switch.h"
 #include "gpio_mcu.h"
-#include "lcditse0803.h"
 #include "timer_mcu.h"
+#include "ble_mcu.h"
+
 /*==================[macros and definitions]=================================*/
-bool encendido = false;
-uint8_t tiempoRespuesta;
+
+#define CONFIG_MEDICION_ms 20
+#define CONFIG_TECLAS_us 1000
+#define LENGTH_SECUENCIA 15
 
 /*==================[internal data definition]===============================*/
 
-/** @fn encenderLed (void *parametro)
+uint16_t tiempoRespuesta;
+uint16_t tiempoRespuestaTotal = 0;
+
+bool encendido = false;
+
+bool inicio = false;
+
+uint8_t ledActual;
+
+uint8_t iterador;
+
+uint8_t secuencia[LENGTH_SECUENCIA] = {2, 1, 2, 1, 1, 2, 2, 1, 2, 0, 1, 0, 0, 0, 2};
+
+typedef struct
+{
+	gpio_t led;
+	gpio_t tecla;
+} leds;
+
+leds misLeds[3] = {
+	{GPIO_11, GPIO_1},
+	{GPIO_10, GPIO_2},
+	{GPIO_5, GPIO_3}};
+
+TaskHandle_t medir_task_handle = NULL;
+TaskHandle_t inicio_task_handle = NULL;
+TaskHandle_t salidaBluetooth_task_handle = NULL;
+
+/*==================[internal functions declaration]=========================*/
+
+void FuncTimerA(void *parametro)
+{
+	vTaskNotifyGiveFromISR(inicio_task_handle, pdFALSE); /* Envía una notificación a la tarea asociada a medir */
+	vTaskNotifyGiveFromISR(medir_task_handle, pdFALSE);	 /* Envía una notificación a la tarea asociada a medir */
+}
+
+/** @fn salidaBluetooth (void *parametro)
+ * @brief tarea encargada de devolver por bluetooth el parametro medido */
+void salidaBluetooth()
+{
+	char mensaje[200];
+
+	sprintf(mensaje, "El tiempo de respuesta promedio es: %d\r\n", tiempoRespuestaTotal);
+
+	if (BleStatus() == BLE_CONNECTED)
+		BleSendString(mensaje);
+}
+
+/** @fn controlLed (void *parametro)
  * @brief tarea encargada de encender los leds de manera random */
-static void encenderLed(void *vparametro)
+static void controlLed(void *vparametro)
 {
 	while (true)
 	{
-		if (encendido == false)
+		if (inicio)
 		{
-			// encender 1 led  y cambiar encendido por true
-			//medir ();
-			//encendido=false;
+			if (encendido == false)
+			{
+				if (iterador == LENGTH_SECUENCIA)
+				{
+					GPIOOff(misLeds[ledActual].led);
+					inicio = false;
+					tiempoRespuestaTotal = tiempoRespuestaTotal/LENGTH_SECUENCIA;
+					printf("%d\r\n", tiempoRespuestaTotal);
+					salidaBluetooth();
+				}
+				else
+				{
+					if (iterador > 0)
+						GPIOOff(misLeds[ledActual].led);
+
+					ledActual = secuencia[iterador];
+					GPIOOn(misLeds[ledActual].led);
+					encendido = true;
+					iterador++;
+				}
+			}
 		}
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	}
 }
 
 /** @fn medir (void *parametro)
  * @brief tarea encargada de medir el timepo desde que se enciende el led hasta que es apagado por
  * el pulsador */
-static void medir (void *vparametro)
+static void medir(void *vparametro)
 {
-	//contador encendido apagado segundos
-	//cambiar encendido por false
-	//salidaBluetooth();
+	uint16_t contador = 0;
+	while (true)
+	{
+		if (inicio)
+		{
+			if (encendido)
+			{
+				if (!GPIORead(misLeds[ledActual].tecla))
+				{
+					tiempoRespuesta = (contador * CONFIG_TECLAS_us) / 1000;
+					tiempoRespuestaTotal = tiempoRespuestaTotal + tiempoRespuesta;
+					encendido = false;
+					contador = 0;
+				}
+				else
+				{
+					contador++;
+				}
+			}
+		}
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+	}
 }
 
-/** @fn salidaBluetooth (void *parametro)
- * @brief tarea encargada de devolver por bluetooth el parametro medido */
-static void salidaBluetooth (void *vparametro)
+/** @fn tecla_1 (void *parametro)
+ * @brief funcion que se encarga de controlar la tecla 1 */
+void tecla_1()
 {
-	// salir por bluetooth el valor tiempo en respuesta
+	inicio = true;
+	iterador = 0;
 }
 
-/*==================[internal functions declaration]=========================*/
+void read_data(uint8_t *data, uint8_t length)
+{
+}
 
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
-	printf("Hello world!\n");
+	iterador = 0;
+
+	SwitchesInit();
+	SwitchActivInt(SWITCH_1, tecla_1, NULL);
+
+	ble_config_t ble_configuration = {
+		"ESP_EDU_1",
+		read_data};
+
+	timer_config_t timer_inicio = {
+		.timer = TIMER_A,
+		.period = CONFIG_TECLAS_us,
+		.func_p = FuncTimerA,
+		.param_p = NULL};
+	TimerInit(&timer_inicio);
+
+	for (uint8_t i=0; i<3; i++)
+	{
+		GPIOInit(misLeds[i].led, GPIO_OUTPUT);
+		GPIOInit(misLeds[i].tecla, GPIO_INPUT);
+	}
+	xTaskCreate(&controlLed, "ControlaLeds", 2048, NULL, 5, &inicio_task_handle);
+	xTaskCreate(&medir, "medirTiempo", 2048, NULL, 5, &medir_task_handle);
+	// xTaskCreate(&salidaBluetooth, "muestraTiempo", 2048, NULL, 5, &salidaBluetooth_task_handle);
+
+	TimerStart(timer_inicio.timer);
+
+	BleInit(&ble_configuration);
 }
 /*==================[end of file]============================================*/
